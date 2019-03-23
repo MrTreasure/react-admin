e3d6ae7a496f94e3b6d0deb1815a87eb70983c2c
Treasure <treasurewmj@foxmail.com>
2019-03-24 01:18:44 +0800

    feat:添加基本路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import styles from './App.module.scss';
-import UserInfo from '@/components/UserInfo'
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import Layout from '@/views/Layout'
+import Login from '@/views/Login'
+import NotFound from '@/views/NotFound'
 class App extends Component {
   render() {
     return (
-      <div className={styles['App']}>
-        <header className={styles['App-header']}>
-          <img src={logo} className={styles['App-logo']} alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <UserInfo age={2}/>
-        </header>
-      </div>
-    );
+      <Router>
+        <Switch>
+          <Route path="/login" component={Login}></Route>
+          <Route path="/404" component={NotFound}></Route>
+          <Route path="/" component={Layout}></Route>
+        </Switch>
+      </Router>
+    )
   }
 }
 
-export default App;
+export default App
