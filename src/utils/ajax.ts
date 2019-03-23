1dc2e6712f2973a89c7ece1ab250beed8f66ee99
Treasure <treasurewmj@foxmail.com>
2019-03-23 16:49:55 +0800

    feat:添加ajax请求工具

---
+++ src/utils/ajax.ts
@@ -0,0 +1,11 @@
+import axios from 'axios'
+import { BASE_URL } from '@/config'
+
+export default axios.create({
+  withCredentials: true,
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json;charset=UTF-8'
+  },
+  timeout: 3000
+})
