37124d70110b5b3dcb340968bd7ac2c95119b552
Treasure <treasurewmj@foxmail.com>
2019-03-23 14:53:39 +0800

    chore:antd按需加载&覆盖配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import styles from './App.module.scss';
+import { Button } from 'antd'
 
 class App extends Component {
   render() {
@@ -11,14 +12,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
-          <a
-            className={styles['App-link']}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <Button>Learn React</Button>
         </header>
       </div>
     );
