d7282d2875dab8469553da2195c5c11c0d248d03
Treasure <treasurewmj@foxmail.com>
2019-03-24 10:52:16 +0800

    temp:调整引用顺序

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,5 @@
-import { combineReducers } from 'redux'
 import UserInfoReducer, { IUserInfo } from './UserInfo'
+import { combineReducers } from 'redux'
 
 export interface IRootState {
   user: IUserInfo
