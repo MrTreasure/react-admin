c2631cfbe5b56a8fbc67840f642ab40dc969f3a3
Treasure <treasurewmj@foxmail.com>
2019-04-01 23:27:38 +0800

    feat:添加脑图&修复图表创建时机

--- src/views/Editor/GGEditor/Flow/index.tsx
+++ src/views/Editor/GGEditor/Flow/index.tsx
@@ -7,33 +7,69 @@ import { FlowContextMenu } from '../components/EditorContextMenu'
 import { FlowDetailPanel } from '../components/EditorDetailPanel'
 import { FlowItemPanel } from '../components/EditorItemPanel'
 import { FlowToolbar } from '../components/EditorToolbar'
-import './index.scss'
 
 GGEditor.setTrackable(false)
 
-const FlowPage = () => {
-  return (
-    <GGEditor className={styles.editor}>
-      <Row type="flex" className={styles.editorHd}>
-        <Col span={24}>
-          <FlowToolbar />
-        </Col>
-      </Row>
-      <Row type="flex" className={styles.editorBd}>
-        <Col span={4} className={styles.editorSidebar}>
-          <FlowItemPanel />
-        </Col>
-        <Col span={16} className={styles.editorContent}>
-          <Flow style={{width: '100%', height: '100%'}} className={styles.flow}/>
-        </Col>
-        <Col span={4} className={styles.editorSidebar}>
-          <FlowDetailPanel />
-          <EditorMinimap />
-        </Col>
-      </Row>
-      <FlowContextMenu />
-    </GGEditor>
-  )
+// const FlowPage = () => {
+//   return (
+//     <GGEditor className={styles.editor}>
+//       <Row type="flex" className={styles.editorHd}>
+//         <Col span={24}>
+//           <FlowToolbar />
+//         </Col>
+//       </Row>
+//       <Row type="flex" className={styles.editorBd}>
+//         <Col span={4} className={styles.editorSidebar}>
+//           <FlowItemPanel />
+//         </Col>
+//         <Col span={16} className={styles.editorContent}>
+//           <Flow style={{width: 1100, height: 820}} className={styles.flow}/>
+//         </Col>
+//         <Col span={4} className={styles.editorSidebar}>
+//           <FlowDetailPanel />
+//           <EditorMinimap />
+//         </Col>
+//       </Row>
+//       <FlowContextMenu />
+//     </GGEditor>
+//   )
+// }
+
+class FlowPage extends React.Component {
+  public state = {
+    visible: false
+  }
+
+  public componentDidMount() {
+    this.setState({
+      visible: true
+    })
+  }
+
+  public render() {
+    return (
+      <GGEditor className={styles.editor}>
+        <Row type="flex" className={styles.editorHd}>
+          <Col span={24}>
+            <FlowToolbar />
+          </Col>
+        </Row>
+        <Row type="flex" className={styles.editorBd}>
+          <Col span={4} className={styles.editorSidebar}>
+            <FlowItemPanel />
+          </Col>
+          <Col span={16} className={styles.editorContent}>
+            {this.state.visible ? <Flow style={{width: '100%', height: '100%'}} className={styles.flow}/> : null}
+          </Col>
+          <Col span={4} className={styles.editorSidebar}>
+            <FlowDetailPanel />
+            <EditorMinimap />
+          </Col>
+        </Row>
+        <FlowContextMenu />
+      </GGEditor>
+    )
+  }
 }
 
 export default FlowPage
--- src/views/Editor/GGEditor/Mind/index.tsx
+++ src/views/Editor/GGEditor/Mind/index.tsx
@@ -8,28 +8,62 @@ import { MindContextMenu } from '../components/EditorContextMenu'
 import { MindDetailPanel } from '../components/EditorDetailPanel'
 import { MindToolbar } from '../components/EditorToolbar'
 
-(GGEditor as any).setTrackable(false);
+GGEditor.setTrackable(false);
 
-const MindPage = () => {
-  return (
-    <GGEditor className={styles.editor}>
-      <Row type="flex" className={styles.editorHd}>
-        <Col span={24}>
-          <MindToolbar />
-        </Col>
-      </Row>
-      <Row type="flex" className={styles.editorBd}>
-        <Col span={20} className={styles.editorContent}>
-          <Mind data={data} className={styles.mind} />
-        </Col>
-        <Col span={4} className={styles.editorSidebar}>
-          <MindDetailPanel />
-          <EditorMinimap />
-        </Col>
-      </Row>
-      <MindContextMenu />
-    </GGEditor>
-  )
+// const MindPage = () => {
+  // return (
+  //   <GGEditor className={styles.editor}>
+  //     <Row type="flex" className={styles.editorHd}>
+  //       <Col span={24}>
+  //         <MindToolbar />
+  //       </Col>
+  //     </Row>
+  //     <Row type="flex" className={styles.editorBd}>
+  //       <Col span={20} className={styles.editorContent}>
+  //         <Mind data={data} className={styles.mind} />
+  //       </Col>
+  //       <Col span={4} className={styles.editorSidebar}>
+  //         <MindDetailPanel />
+  //         <EditorMinimap />
+  //       </Col>
+  //     </Row>
+  //     <MindContextMenu />
+  //   </GGEditor>
+  // )
+// }
+
+class MindPage extends React.Component {
+  public state = {
+    visible: false
+  }
+
+  public componentDidMount() {
+    this.setState({
+      visible: true
+    })
+  }
+
+  public render() {
+    return (
+      <GGEditor className={styles.editor}>
+        <Row type="flex" className={styles.editorHd}>
+          <Col span={24}>
+            <MindToolbar />
+          </Col>
+        </Row>
+        <Row type="flex" className={styles.editorBd}>
+          <Col span={20} className={styles.editorContent}>
+          {this.state.visible ? <Mind style={{width: '100%', height: '100%'}} className={styles.flow}/> : null}
+          </Col>
+          <Col span={4} className={styles.editorSidebar}>
+            <MindDetailPanel />
+            <EditorMinimap />
+          </Col>
+        </Row>
+        <MindContextMenu />
+      </GGEditor>
+    )
+  }
 }
 
 export default MindPage
