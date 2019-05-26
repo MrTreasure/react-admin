6510a7c7cdcd2c103e220f1313d000b5afb4f3d1
Treasure <treasurewmj@foxmail.com>
2019-05-27 00:37:43 +0800

    feat:监控页添加基本图表

--- src/views/Dashboard/Monitor/index.tsx
+++ src/views/Dashboard/Monitor/index.tsx
@@ -1,10 +1,51 @@
 import * as React from 'react'
+import Chart from '@/components/Chart'
+import './index.scss'
+import produce from 'immer'
 
-export default class Monitor extends React.Component {
+const mock = () => {
+  return {
+    xAxis: {
+        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+    },
+    yAxis: {},
+    series: [{
+      name: '销量',
+      type: 'bar',
+      data: [5, 20, 36, 10, 10, 20],
+      label: {
+        show: true
+      }
+    }]
+  }
+}
+
+export default class Monitor extends React.Component<any, any> {
+
+  public state = {
+    options: mock()
+  }
+
+  public componentDidMount() {
+    const ticker = window.setInterval(() => {
+      this.setState({
+        options: produce(this.state.options, (opt) => {
+          opt.series[0].data[0] = opt.series[0].data[0] + 5
+        })
+      })
+    }, 2000)
+
+    window.setTimeout(() => {
+      window.clearInterval(ticker)
+    }, 20000)
+  }
+
   public render() {
     return (
       <div className="monitor">
-        监控页
+        <div className="chart-wrapper">
+          <Chart options={this.state.options} debug={true}/>
+        </div>
       </div>
     )
   }
