495622919f3b5b16c4b6a9640e4bce4981caddc8
Treasure <treasurewmj@foxmail.com>
2019-07-07 11:23:13 +0800

    feat:添加基本操作

--- src/views/Dashboard/WorkBench/index.tsx
+++ src/views/Dashboard/WorkBench/index.tsx
@@ -1,9 +1,26 @@
 import * as React from 'react'
+import { Col, Row, Card, Button } from 'antd'
+
+const ButtonGroup = Button.Group
 
 export default class WorkBench extends React.Component {
   public render() {
     return <div className="work-bench">
+      <Row gutter={12}>
+        <Col span={12}>
+          <Card title="操作">
+            <ButtonGroup>
+              <Button>开始</Button>
+              <Button>中断</Button>
+            </ButtonGroup>
+          </Card>
+        </Col>
 
+        <Col span={12}>
+          <Card title="结果页">
+          </Card>
+        </Col>
+      </Row>
     </div>
   }
 }
